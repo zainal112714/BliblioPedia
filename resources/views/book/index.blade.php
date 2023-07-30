1b8564705e4ed7a33dc6a7a3771a4d8aa32977e6
Novi <novimonicahutagalung@gmail.com>
2023-07-30 10:53:18 +0700

    edit and update

--- resources/views/book/index.blade.php
+++ resources/views/book/index.blade.php
@@ -41,7 +41,7 @@
                         <th>Penulis</th>
                         <th>Penerbit</th>
                         <th>Sinopsis</th>
-                        <th></th>
+                        <th>Action</th>
                     </tr>
                 </thead>
             </table>
@@ -92,7 +92,7 @@
                         <td>{{ $book->synopsis }}</td>
                         <td>
                             <div class="d-flex">
-                                <a href="{{ route('books.show', ['book' => $book->id]) }}" class="btn btn-outline-dark btn-sm me-2"><i class="bi-person-lines-fill"></i></a>
+                                <a href="{{ route('books.show', ['book' => $book->]) }}" class="btn btn-outline-dark btn-sm me-2"><i class="bi-person-lines-fill"></i></a>
                                 <a href="" class="btn btn-outline-dark btn-sm me-2"><i class="bi-pencil-square"></i></a>
 
                                 <div>
