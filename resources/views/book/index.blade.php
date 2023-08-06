2b86672138831dcfd6e3c29a55d7bf48a58a86a1
NoviaKartika01 <noviakartikaawalia@gmail.com>
2023-08-07 00:06:30 +0700

    ganti kata loan dengan buku

--- resources/views/book/index.blade.php
+++ resources/views/book/index.blade.php
@@ -22,7 +22,7 @@
                         <li class="list-inline-item">|</li>
                         <li class="list-inline-item">
                             <a href="{{ route('books.create') }}" class="btn btn-primary">
-                                <i class="bi bi-plus-circle me-1"></i> Make Loan
+                                <i class="bi bi-plus-circle me-1"></i> Add Book
                             </a>
                         </li>
                     </ul>
